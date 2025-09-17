8a4aa0936dcba7a134a6453ed7d96f10e62338cb
Severitech <padilladouglas6@gmail.com>
2025-09-17 01:28:14 -0400

    modificando los usuarios

--- backend/settings.py
+++ backend/settings.py
@@ -75,17 +75,22 @@ USE_TZ = True
 
 
 # settings.py
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'xDFPNklpQdkPEFRYjdMmdDGDKmzmgGff',
+#         'HOST': 'switchyard.proxy.rlwy.net',
+#         'PORT': '21139',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'xDFPNklpQdkPEFRYjdMmdDGDKmzmgGff',
-        'HOST': 'switchyard.proxy.rlwy.net',
-        'PORT': '21139',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 STATIC_URL = "/static/"
