037585ea8a5fe653f3a437fd6d29f271150fe3c9
Roly <lijeronhr10@gmail.com>
2025-09-17 01:41:52 -0400

    cambios

--- backend/settings.py
+++ backend/settings.py
@@ -75,14 +75,21 @@ USE_TZ = True
 
 
 # settings.py
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'xDFPNklpQdkPEFRYjdMmdDGDKmzmgGff',
+#         'HOST': 'switchyard.proxy.rlwy.net',
+#         'PORT': '21139',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'xDFPNklpQdkPEFRYjdMmdDGDKmzmgGff',
-        'HOST': 'switchyard.proxy.rlwy.net',
-        'PORT': '21139',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
