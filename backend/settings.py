7a65aa58a5dd704e718631f885432807d090f84d
hebertsb <suarezburgoshebert@gmail.com>
2025-09-18 01:47:35 -0400

    Cargado De Reserva

--- backend/settings.py
+++ backend/settings.py
@@ -75,16 +75,23 @@ USE_TZ = True
 
 
 # settings.py
+# DATABASES = {
+ #     'default': {
+  #         'ENGINE': 'django.db.backends.postgresql',
+ #          'NAME': 'railway',
+ #          'USER': 'postgres',
+ #         'PASSWORD': 'xDFPNklpQdkPEFRYjdMmdDGDKmzmgGff',
+ #        'HOST': 'switchyard.proxy.rlwy.net',
+ #          'PORT': '21139',
+ #     }
+#  }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'xDFPNklpQdkPEFRYjdMmdDGDKmzmgGff',
-        'HOST': 'switchyard.proxy.rlwy.net',
-        'PORT': '21139',
+    'default' :{
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
     }
-}
+ }
 
 
 
--- catalogo/serializers.py
+++ catalogo/serializers.py
@@ -31,3 +31,5 @@ class PaqueteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Paquete
         fields = "__all__"
+
+    max_personas = serializers.IntegerField(read_only=True)
