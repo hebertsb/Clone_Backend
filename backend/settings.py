f5a700b1839047bc6aef90902f59f6eba1c9a21c
Severitech <padilladouglas6@gmail.com>
2025-09-18 04:14:51 -0400

    Produccion

--- backend/settings.py
+++ backend/settings.py
@@ -92,24 +92,25 @@ USE_TZ = True
 
 
 # # settings.py
-# DATABASES = {
-#      'default': {
-#           'ENGINE': 'django.db.backends.postgresql',
-#           'NAME': 'railway',
-#           'USER': 'postgres',
-#          'PASSWORD': 'xDFPNklpQdkPEFRYjdMmdDGDKmzmgGff',
-#          'HOST': 'switchyard.proxy.rlwy.net',
-#           'PORT': '21139',
-#      }
-#  }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'VngKXrFjuUHMtwwryFxYlWMcUuQXWsjw',
+        'HOST': 'yamanote.proxy.rlwy.net',
+        'PORT': '59338',
     }
-
 }
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+
+# }
 
 
 STATIC_URL = "/static/"
